2b4696cf6931d738060d47d08eab77621bbb1132
Jens Schmer <der.liggers@gmail.com>
2013-12-25 12:09:59 +0100

    hotkey edit that can handle keyboard shortcuts

--- PyMangaHotkeyLineEdit.py
+++ PyMangaHotkeyLineEdit.py
@@ -2,13 +2,19 @@ from PyQt5.QtWidgets import (QLineEdit, QDialog)
 from PyQt5.QtCore import (Qt, QEvent)
 from PyQt5.QtGui import QKeySequence
 
+from PyMangaLogger import log
 from ui_hotkey import Ui_HotkeyDialog
 
 class HotkeyLineEdit(QLineEdit):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         super(HotkeyLineEdit, self).__init__(parent)
 
+    def mousePressEvent(self, event):
+        log.info("Mouse press")
+        # TODO: construct a data structure to hold mouse presses that can be used as shortcuts
+
     def keyPressEvent(self, event):
+        log.info("Key Press")
         key = event.key(); 
         key = Qt.Key(key); 
 
@@ -21,8 +27,8 @@ class HotkeyLineEdit(QLineEdit):
             self.setText(None)
             return
         
-        # Empty means a special key like F5, Delete, Home etc
-        if len(event.text()) == 0:
+        # modifier keys can't stand alone
+        if key in (Qt.Key_Control, Qt.Key_Alt, Qt.Key_AltGr, Qt.Key_Shift):
             return
 
         # Checking for key combinations
@@ -31,13 +37,13 @@ class HotkeyLineEdit(QLineEdit):
         if(modifiers & Qt.NoModifier):
             return
         if(modifiers & Qt.ShiftModifier):
-            key += Qt.SHIFT; 
+            key += Qt.SHIFT
         if(modifiers & Qt.ControlModifier):
-            key += Qt.CTRL; 
+            key += Qt.CTRL 
         if(modifiers & Qt.AltModifier):
-            key += Qt.ALT; 
+            key += Qt.ALT 
 
-        self.setText( QKeySequence(key).toString(QKeySequence.NativeText) )
+        self.setText(QKeySequence(key).toString(QKeySequence.NativeText))
 
 class HotkeyDialog(QDialog):
 
@@ -50,5 +56,9 @@ class HotkeyDialog(QDialog):
 
         self.setAttribute(Qt.WA_DeleteOnClose)
 
-        # connect buttons
-        self.ui.edit_hotkey = HotkeyLineEdit(self)
+        # add hotkey edits
+        self.ui.formLayout.addRow("Next Page", HotkeyLineEdit())
+        self.ui.formLayout.addRow("Previous Page", HotkeyLineEdit())
+        self.ui.formLayout.addRow("Toggle Fullscreen", HotkeyLineEdit())
+
+    # TODO: save shortcuts in easy accessable container when the dialog is closed
